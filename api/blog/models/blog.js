a3280f061ddee203b69d4413fc996a99895b5a63
Süleyman Özdemir <33416440+suleymanozdemir@users.noreply.github.com>
2021-12-09 21:12:13 +0300

    add slug system

--- api/blog/models/blog.js
+++ api/blog/models/blog.js
@@ -1,8 +1,16 @@
-'use strict';
+const slugify = require("slugify");
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
- * to customize this model
- */
-
-module.exports = {};
+module.exports = {
+  lifecycles: {
+    beforeCreate: async (data) => {
+      if (data.title) {
+        data.slug = slugify(data.title);
+      }
+    },
+    beforeUpdate: async (params, data) => {
+      if (data.title) {
+        data.slug = slugify(data.title);
+      }
+    },
+  },
+};
--- config/plugins.js
+++ config/plugins.js
@@ -1,12 +1,10 @@
 module.exports = ({ env }) => ({
-  // ...
-  upload: {
-    provider: "cloudinary",
-    providerOptions: {
-      cloud_name: env("CLOUDINARY_NAME"),
-      api_key: env("CLOUDINARY_KEY"),
-      api_secret: env("CLOUDINARY_SECRET"),
-    },
-  },
-  // ...
+   upload: {
+     provider: "cloudinary",
+     providerOptions: {
+       cloud_name: env("CLOUDINARY_NAME"),
+       api_key: env("CLOUDINARY_KEY"),
+       api_secret: env("CLOUDINARY_SECRET"),
+     },
+   },
 });
