5b6723d6b9e585a87dc4c8b0186e0dcd719454ea
Süleyman Özdemir <33416440+suleymanozdemir@users.noreply.github.com>
2021-12-09 22:33:15 +0300

    delete strapi v3

--- config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '41aca6ec67af676ccb2c6bd5618b2c9e'),
-    },
-  },
-});
--- extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '6d7de40d-4cba-47e0-94d4-eb4c15f3c8a1'
-};
