405e5fc1c79ae28cca1aba6e64a922d0c8605a07
suleyman.ozdemir <suleyman.ozdemir@agmlab.com>
2022-11-08 19:40:40 +0300

    updates

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,16 +1,17 @@
 module.exports = ({ env }) => ({
     connection: {
-        client: 'postgres',
-        connection: {
-            host: env('DATABASE_HOST'),
-            port: env.int('DATABASE_PORT'),
-            database: env('DATABASE_NAME'),
-            user: env('DATABASE_USERNAME'),
-            password: env('DATABASE_PASSWORD'),
-            ssl: {
-                rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
-            },
+      client: 'postgres', 
+    connection: {
+          host: env('DATABASE_HOST', '127.0.0.1'),
+          port: env.int('DATABASE_PORT', 5432),
+          database: env('DATABASE_NAME', 'strapi'),
+          user: env('DATABASE_USERNAME', ''),
+          password: env('DATABASE_PASSWORD', ''),
+          ssl: {
+            rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
+         },
         },
         debug: false,
     },
-});
+  });
+   
