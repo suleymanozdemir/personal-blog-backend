5c2f79cac9290c7cbc5e74b357fb660d4d73a324
Süleyman Özdemir <33416440+suleymanozdemir@users.noreply.github.com>
2023-04-17 17:52:26 +0300

    migregate by strapi 4

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
     client: "postgres",
     connection: {
       host: env("DATABASE_HOST", "127.0.0.1"),
-      port: env.int("DATABASE_PORT", 5432),
+      port: env.int("DATABASE_PORT", 5433),
       database: env("DATABASE_NAME", "personal-blog-DB"),
       user: env("DATABASE_USERNAME", "suleymanozdemir"),
       password: env("DATABASE_PASSWORD", "7387381"),
